1dd2d035d1f415b1c930f68e9420e9cb6ed63844
Jared Marsh <jared.marsh@hotmail.co.uk>
2020-12-16 12:43:55 +0000

    Updated SQL File

--- ab_coursework.sql
+++ ab_coursework.sql
@@ -1,30 +1,29 @@
 DROP DATABASE IF EXISTS 'abcoursework_db';
 
-CREATE TABLE IF NOT EXISTS 'abcousework_db';
+CREATE DATABASE 'abcoursework_db' COLLATE utf8_unicode_ci;
 
-GRANT SELECT,INSERT ON abcoursework_db.* TO 'abcoursework_user'@localhost IDENTIFIED BY 'abcoursework_pass';
+CREATE USER IF NOT EXISTS 'abcoursework_user'@'localhost' IDENTIFIED BY 'abcoursework_pass';
+GRANT SELECT,INSERT ON abcoursework_db.* TO 'abcoursework_user'@localhost;
 
+USE abcoursework_db;
 
-USE ab_coursework.db;
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+	`id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+	`username` varchar(256) NOT NULL ,
+	`password` varchar(256) NOT NULL ,
+	`phone` int(13) NOT NULL ,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS 'users'(
-	'key' int(100) unsigned NOT NULL AUTO_INCREMENT,
-	'username' varchar(256) NOT NULL ,
-	'password' varchar(256) NOT NULL ,
-	'phone' int(13) NOT NULL ,
-	PRIMARY KEY ('key')
-	)
-	
-CREATE TABLE IF NOT EXISTS 'messages'(
-	'key' int(100) unsigned NOT NULL AUTO_INCREMENT,
-	'source' int(13) NOT NULL,
-	'destination' int(13) NOT NULL,
-	'recieved' timestamp NOT NULL,
-	'bearer' varchar(4) NOT NULL,
-	'ref' int(100) NOT NULL,
-	'message' text NOT NULL,
-	PRIMARY KEY ('key')
-	)
-	
-	
-	
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+	`id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+	`source` int(13) NOT NULL,
+	`destination` int(13) NOT NULL,
+	`received` timestamp NOT NULL,
+	`bearer` varchar(4) NOT NULL,
+	`ref` int(100) NOT NULL,
+	`message` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
