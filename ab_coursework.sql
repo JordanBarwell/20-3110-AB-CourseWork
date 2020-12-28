bed1f72dcd6703919fd1644bbfd2b30864075bbe
Jared Marsh <jared.marsh@hotmail.co.uk>
2020-12-28 13:08:30 +0000

    Update ab_coursework.sql
    Adds in email column, makes both id columns into BIGINTS as unsigned ints aren't possible in PHP.

--- ab_coursework.sql
+++ ab_coursework.sql
@@ -9,21 +9,22 @@ USE abcoursework_db;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-	`id` int(100) unsigned NOT NULL AUTO_INCREMENT,
-	`username` varchar(256) NOT NULL ,
-	`password` varchar(256) NOT NULL ,
-	`phone` int(13) NOT NULL ,
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(256) NOT NULL,
+	`password` VARCHAR(256) NOT NULL,
+	`email` VARCHAR(300) NOT NULL,
+	`phone` INT(13) NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
-	`id` int(100) unsigned NOT NULL AUTO_INCREMENT,
-	`source` int(13) NOT NULL,
-	`destination` int(13) NOT NULL,
-	`received` timestamp NOT NULL,
-	`bearer` varchar(4) NOT NULL,
-	`ref` int(100) NOT NULL,
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`source` INT(13) NOT NULL,
+	`destination` INT(13) NOT NULL,
+	`received` TIMESTAMP NOT NULL,
+	`bearer` VARCHAR(4) NOT NULL,
+	`ref` INT NOT NULL,
 	`message` TEXT NOT NULL,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
