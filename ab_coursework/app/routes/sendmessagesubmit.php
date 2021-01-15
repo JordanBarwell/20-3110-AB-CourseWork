bce36f0880c248302ef8d6965bdec26db01960ba
JordanBarwell <58300682+JordanBarwell@users.noreply.github.com>
2021-01-15 13:23:00 +0000

    Add files via upload

--- ab_coursework/app/routes/sendmessagesubmit.php
+++ ab_coursework/app/routes/sendmessagesubmit.php
@@ -7,7 +7,55 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app->post('/sendmessagesubmit', function (Request $request, Response $response) use ($app) {
 
     $input = $request->getParsedBody();
-    $msgToSend = $input['Message'];
+    $tempMsg = 'Temperature is: ' . $input['Temperature'];
+    $fanDirectionMsg = '';
+    $lastDigitMsg = 'Last Digit Entered: ' . $input['KeyPad'];
+    $switchMsg1 = '';
+    $switchMsg2 = '';
+    $switchMsg3 = '';
+    $switchMsg4 = '';
+
+    if ($input['radioFan'] === "Forward"){
+          $fanDirectionMsg = 'Fan direction: Forwards';
+    } elseif ($input['radioFan'] === "Reverse"){
+        $fanDirectionMsg = 'Fan direction: Reverse';
+    }
+
+    if ($input['radioSwitch1'] === "switchOneON"){
+        $switchMsg1 = 'Switch One is: ON';
+    } elseif ($input['radioSwitch1'] === "switchOneOFF"){
+        $switchMsg1 = 'Switch One is: OFF';
+    }
+
+    if ($input['radioSwitch2'] === "switchTwoON"){
+        $switchMsg2 = 'Switch Two is: ON';
+    } elseif ($input['radioSwitch2'] === "switchTwoOFF"){
+        $switchMsg2 = 'Switch Two is: OFF';
+    }
+
+    if ($input['radioSwitch3'] === "switchThreeON"){
+        $switchMsg3 = 'Switch Three is: ON';
+    } elseif ($input['radioSwitch3'] === "switchThreeOFF"){
+        $switchMsg3 = 'Switch Three is: OFF';
+    }
+
+    if ($input['radioSwitch4'] === "switchFourON"){
+        $switchMsg4 = 'Switch Four is: ON';
+    } elseif ($input['radioSwitch4'] === "switchFourOFF"){
+        $switchMsg4 = 'Switch Four is: OFF';
+    }
+
+    $msgToSend = [
+        $tempMsg,
+        $fanDirectionMsg,
+        $lastDigitMsg,
+        $switchMsg1,
+        $switchMsg2,
+        $switchMsg3,
+        $switchMsg4
+    ];
+
+    $stringMsg = implode("\n", $msgToSend);
 
     $soapWrapper = $this->get('SoapWrapper');
 
@@ -19,11 +67,12 @@ $app->post('/sendmessagesubmit', function (Request $request, Response $response)
         'username' => $userData['username'],
         'password' => $userData['password'],
         'deviceMSISDN' => '+44',
-        'message' => $msgToSend,
+        'message' => $stringMsg,
         'deliveryReport' => 0,
         'mtBearer' => 'SMS'
     ];
 
+    var_dump($stringMsg);
     $sendMsg = $soapWrapper->performSoapFunction($userID, 'sendMessage', $params);
 
     return $this->view->render($response,
@@ -32,7 +81,8 @@ $app->post('/sendmessagesubmit', function (Request $request, Response $response)
             'css_path' => CSS_PATH,
             'landing_page' => LANDING_PAGE,
             'page_title' => APP_NAME,
-            'page_heading_1' => 'Message Sent!',
+            'additional_info' => 'Created by Jared, Charlie and Jordan',
+            'page_heading_1' => 'Message Sent!'
         ]);
 
 })->setName('sendmessagesubmit');
